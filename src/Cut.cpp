b589f695acd6cb0bf1ad8278a5e8596c3cb04047
yakomaxa <47655565+yakomaxa@users.noreply.github.com>
2025-12-06 20:37:12 +0900

    if sentence might be optimized in Cut.cpp

--- src/Cut.cpp
+++ src/Cut.cpp
@@ -195,10 +195,10 @@ int Cut::cut(std::vector<Atom>& ca,Domain& dom,CutValues& val,
 	endst = PDPParameters::ENDS;
       };
       
-      if((contact_density[k])<val.s_min && k > from + endsf && k< to - endst) {
-	val.s_min = (contact_density[k]);
-	site_min=k+1;
-      }
+      //      if((contact_density[k])<val.s_min && k > from + endsf && k< to - endst) {
+      //	val.s_min = (contact_density[k]);
+      //	site_min=k+1;
+      //      }
       if(k>from+endsf&&k<to-endst) {
 	if((contact_density[k])<val.s_min){
 	  val.s_min = (contact_density[k]);
